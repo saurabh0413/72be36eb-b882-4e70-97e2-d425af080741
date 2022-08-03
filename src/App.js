43d052d874bd5b1c55ecb3d13a3c9d22bd2c1616
saurabh <saurabh.farkade13@gmail.com>
2022-08-03 23:59:41 +0530

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
+import { useState } from "react";
 import "./App.css";
-// import data from db.json
+import UserDetails from "./components/UserDetails";
 
+import data from "./db.json";
 function App() {
- 
-
+  const [data1, setdata1] = useState(data);
+  function handleAsc() {
+    let data2 = data1.sort((a, b) => {
+      if (a.first_name > b.first_name) return 1;
+      else return -1;
+    });
+    setdata1([...data2]);
+  }
+  function handleDec() {
+    let data2 = data1.sort((a, b) => {
+      if (a.first_name > b.first_name) return -1;
+      else return 1;
+    });
+    setdata1([...data2]);
+  }
   return (
-    <div className="App" data-testid = 'app'>
-      <button  data-testid = 'sort-asc-btn'>Sort by Asc</button>
-      <button  data-testid = 'sort-desc-btn'>Sort by Desc</button>
+    <div className="App" data-testid="app">
+      <button
+        data-testid="sort-asc-btn"
+        onClick={() => {
+          handleAsc();
+        }}
+      >
+        Sort by Asc
+      </button>
+      <button
+        data-testid="sort-desc-btn"
+        onClick={() => {
+          handleDec();
+        }}
+      >
+        Sort by Desc
+      </button>
       {/*  map through the users data and pass props to the Userdetails component */}
+      {data1.map((item) => {
+        return <UserDetails className="hoverkar" item={item} key={item.id} />;
+      })}
     </div>
   );
 }
--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,28 +1,36 @@
 // import module.css here;
+// import "./userDetails.module.css"
+import styles from "./userDetails.module.css";
 const UserDetails = (props) => {
   return (
     <>
-      <div data-testid="user-container">
+      <div data-testid="user-container" className={styles.mainDiv}>
         {/* user image */}
+        <img className={styles.imgSize} src={props.item.avatar} alt="" />
         <div>
           <div>
-            <h3 data-testid="user-fname"></h3>
-            <h3 data-testid="user-lname"></h3>
+            <h3 data-testid="user-fname">{props.item.first_name}</h3>
+            <h3 data-testid="user-lname">{props.item.last_name}</h3>
           </div>
           <div>
-            <p data-testid="user-address"></p>
+            <p data-testid="user-address">{props.item.address}</p>
           </div>
         </div>
         <div>
-          <h3 data-testid="user-karma"></h3>
+          <h3 data-testid="user-karma">{props.item.karma}</h3>
+          <p>karma</p>
         </div>
         <div>
-          <h3 data-testid="user-followers"></h3>
+          <h3 data-testid="user-followers">{props.item.followers}</h3>
+          <p>followers</p>
         </div>
         <div>
-          <h3 data-testid="user-posts"></h3>
+          <h3 data-testid="user-posts">{props.item.posts}</h3>
+          <p>posts</p>
         </div>
-        <button data-testid="follow-btn"></button>
+        <button data-testid="follow-btn">
+          {props.item.is_following ? "Follow" : "Unfollow"}
+        </button>
       </div>
     </>
   );
